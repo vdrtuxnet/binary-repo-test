48b8821706edbaff48a2fa97e0c12003a008c6f5
vdrtuxnet <felix.l.kamps@gmail.com>
2017-07-14 12:58:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# binary-repo-test
+Binary Addon Test Repo for Kodi 17 &amp; 18, this a developer repo.
+
+I strongly recommend that you only use it under controlled conditions and for testing and debugging.
---
+++ update.sh
@@ -0,0 +1,8 @@
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Generic_x68_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/OSX_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Odroid_C2
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/S905
+
